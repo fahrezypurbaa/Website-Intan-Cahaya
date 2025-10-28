8d51c82f80a2ccc450ad9b74c6c2e0fe07d073ab
Fahrezy Purba <fahrezyromeropurba@gmail.com>
2025-10-28 16:02:27 +0700

    rapiin baris

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Article extends Model
 {
@@ -13,18 +12,16 @@ class Article extends Model
         'meta_title', 'meta_description',
     ];
 
-    // Relasi ke User (opsional)
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Hitung waktu baca otomatis (200 kata per menit)
     public function getReadingTimeAttribute()
     {
         $wordCount = str_word_count(strip_tags($this->content));
         $minutes = ceil($wordCount / 200);
 
-        return $minutes . ' menit baca';
+        return $minutes.' menit baca';
     }
 }
