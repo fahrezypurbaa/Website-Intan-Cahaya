62d9ad57b90c29eba34ce92b6cbd940e679fed74
Fahrezy Purba <fahrezyromeropurba@gmail.com>
2025-12-08 10:49:18 +0700

    fix URL parameters in navbar layanan

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -118,32 +118,48 @@
                                 clip-rule="evenodd"></path>
                         </svg>
                     </button>
+
                     <div
                         class="absolute left-0 mt-2 w-[500px] bg-white shadow-lg rounded-lg border opacity-0 invisible 
                         group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out 
                         transform translate-y-2 group-hover:translate-y-0 grid grid-cols-2 gap-6 p-6">
+
                         <div class="flex flex-col space-y-3">
-                            <a href="{{ route('layanan.index', ['category' => 'sertifikasi-kemnaker-ri']) }}"
-                                class="text-sm hover:text-[#73BA7D] transition-colors">Sertifikasi
-                                Kemnaker RI</a>
-                            <a href="{{ route('layanan.index', ['category' => 'sertifikasi-bnsp']) }}"
-                                class="text-sm hover:text-[#73BA7D] transition-colors">Sertifikasi
-                                BNSP</a>
-                            <a href="{{ route('layanan.index', ['category' => 'non-sertifikasi']) }}"
-                                class="text-sm hover:text-[#73BA7D] transition-colors">Non Sertifikasi</a>
-                            <a href="{{ route('layanan.index', ['category' => 'ppsdm-migas']) }}"
-                                class="text-sm hover:text-[#73BA7D] transition-colors">PPSDM Migas</a>
+                            <a href="/layanan/sertifikasi-kemnaker-ri"
+                                class="text-sm hover:text-[#73BA7D] transition-colors">
+                                Sertifikasi Kemnaker RI
+                            </a>
+
+                            <a href="/layanan/sertifikasi-bnsp" class="text-sm hover:text-[#73BA7D] transition-colors">
+                                Sertifikasi BNSP
+                            </a>
+
+                            <a href="/layanan/non-sertifikasi" class="text-sm hover:text-[#73BA7D] transition-colors">
+                                Non Sertifikasi
+                            </a>
+
+                            <a href="/layanan/ppsdm-migas" class="text-sm hover:text-[#73BA7D] transition-colors">
+                                PPSDM Migas
+                            </a>
                         </div>
+
                         <div class="flex flex-col space-y-3">
-                            <a href="{{ route('layanan.index', ['category' => 'iso']) }}"
-                                class="text-sm hover:text-[#73BA7D] transition-colors">ISO</a>
-                            <a href="{{ route('layanan.index', ['category' => 'esdm']) }}"
-                                class="text-sm hover:text-[#73BA7D] transition-colors">ESDM</a>
-                            <a href="{{ route('layanan.index', ['category' => 'riksa-uji']) }}"
-                                class="text-sm hover:text-[#73BA7D] transition-colors">Riksa Uji</a>
-                            <a href="{{ route('layanan.index', ['category' => 'perpanjangan-sio-lisensi']) }}"
-                                class="text-sm hover:text-[#73BA7D] transition-colors">Perpanjangan
-                                SIO & Lisensi</a>
+                            <a href="/layanan/iso" class="text-sm hover:text-[#73BA7D] transition-colors">
+                                ISO
+                            </a>
+
+                            <a href="/layanan/esdm" class="text-sm hover:text-[#73BA7D] transition-colors">
+                                ESDM
+                            </a>
+
+                            <a href="/layanan/riksa-uji" class="text-sm hover:text-[#73BA7D] transition-colors">
+                                Riksa Uji
+                            </a>
+
+                            <a href="/layanan/perpanjangan-sio-lisensi"
+                                class="text-sm hover:text-[#73BA7D] transition-colors">
+                                Perpanjangan SIO & Lisensi
+                            </a>
                         </div>
                     </div>
                 </div>
